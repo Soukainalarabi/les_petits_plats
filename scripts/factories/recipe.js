9a2fc76975c5c2522677fc9c8c0d09af40625822
Soukaina Larabi <Soukaina.larabi44@gmail.com>
2023-03-15 01:03:13 +0100

    filtrer par titre et description

--- scripts/factories/recipe.js
+++ scripts/factories/recipe.js
@@ -7,8 +7,12 @@ export default function cardFactory(recipe) {
     let timeCard = document.createElement("div")
     let iconTime = document.createElement("img")
     let timeNumber = document.createElement("p")
+    let detailsRecipe = document.createElement("div")
+    let ingredientsUl = document.createElement("ul")
+    let pDescription = document.createElement("p")
     let getCardDom = () => {
         cardRecipes.setAttribute("class", "card")
+        cardRecipes.setAttribute("id", `recipe-${recipe.id}`)
         imgCard.setAttribute("class", "card-img")
         bodyCard.setAttribute("class", "card-body")
         recipeName.setAttribute("class", "recipe-name")
@@ -17,13 +21,29 @@ export default function cardFactory(recipe) {
         iconTime.setAttribute("src", "assets/clock.png")
         iconTime.setAttribute("alt", "icon watch")
         timeNumber.setAttribute("class", "time-number")
+        detailsRecipe.setAttribute("class", "details-recipes")
+        ingredientsUl.setAttribute("class", "ingredients")
+        pDescription.setAttribute("class", "description")
         cardRecipes.appendChild(imgCard)
         cardRecipes.appendChild(bodyCard)
-        cardRecipes.appendChild(recipeName)
+        bodyCard.appendChild(recipeName)
         recipeName.appendChild(name)
         recipeName.appendChild(timeCard)
         timeCard.appendChild(iconTime)
         timeCard.appendChild(timeNumber)
+        bodyCard.appendChild(detailsRecipe)
+        detailsRecipe.appendChild(ingredientsUl)
+        detailsRecipe.appendChild(pDescription)
+        name.textContent = recipe.name
+        timeNumber.textContent = recipe.time + "min"
+        pDescription.textContent = recipe.description
+        recipe.ingredients.forEach(ingredient => {
+            let ingredientsDetails = document.createElement("li")
+            ingredientsUl.appendChild(ingredientsDetails)
+            let quantityInit = ingredient.quantity ? ":" + ingredient.quantity : ""
+            let unitInit = ingredient.unit ? ingredient.unit : ""
+            ingredientsDetails.textContent = ingredient.ingredient + quantityInit + unitInit
+        });
         return (cardRecipes)
 
     }
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,38 @@
 import recipes from "./data/recipes.js"
 import cardFactory from "./factories/recipe.js"
 let cards = document.querySelector(".cards")
-recipes.forEach(recipe => {
-    let cardFactoryModel = cardFactory()
-    let cardDom = cardFactoryModel.getCardDom()
-    cards.appendChild(cardDom)
-    console.log(recipe.name);
+let inputSearch = document.getElementById("contenu-search")
+inputSearch.addEventListener("search", () => {
+    let cardElms = document.querySelectorAll(".card")
+    if (!inputSearch.value.trim() || inputSearch.value.length < 3) {
+        cardElms.forEach(card => {
+            card.style.display = "block"
+        })
+        return
+    }//pour ne pas effectuer une recherche si la valeur de input est vide et contient des espace
+    cardElms.forEach(card => {
+        card.style.display = "none"
+    })
+    const result = recipes.filter(recipe => recipe.name.toUpperCase().includes(inputSearch.value.toUpperCase().trim()) ||
+        recipe.description.toUpperCase().includes(inputSearch.value.toUpperCase().trim())
+    );
+    console.log(result)
+
+    result.forEach(res => {
+        document.getElementById(`recipe-${res.id}`).style.display = "block"
+
+
+    })
 });
+let showRecipes = () => {
+    recipes.forEach(recipe => {
+        let cardFactoryModel = cardFactory(recipe)
+        let cardDom = cardFactoryModel.getCardDom()
+        cards.appendChild(cardDom)
+
+    });
+
+}
+showRecipes()
+
+
